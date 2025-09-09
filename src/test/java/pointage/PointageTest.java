157790a23cccb270a308c02915c1f301f1471edc
N0mena <alida.rajaonarivelo@gmail.com>
2025-09-09 12:05:47 +0300

    feat: try

--- src/test/java/pointage/PointageTest.java
+++ src/test/java/pointage/PointageTest.java
@@ -2,11 +2,17 @@ package pointage;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.Instant;
+
+import static java.lang.Float.parseFloat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PointageTest {
      private  Pointage pointage1;
 
+     void setUp(){
+         var salaire1 = new Salaire()
+     }
     @Test
     public void testVerifyQuota() {
         var pointage1 = new Pointage("Enseigner", 1,1.4);
@@ -14,6 +20,12 @@ class PointageTest {
 
         assertEquals("Le quota du temps alloué doit être égal à 1", pointage1.verifyQuota());
         assertEquals(true,pointage2.verifyQuota());
+
+    }
+
+    public  Float tesGetTotalRevenu(Instant t)
+    {
+         var revenu1 = TotalSalaire.getTotalRevenu(parseFloat("2025-02-01T08:00:00Z"));
 
     }
 
