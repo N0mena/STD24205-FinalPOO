eeb9c9e2af77dfc3173c0d6c660af55d24f2e86c
N0mena <alida.rajaonarivelo@gmail.com>
2025-09-09 12:00:21 +0300

    feat: try

--- src/test/java/pointage/PointageTest.java
+++ src/test/java/pointage/PointageTest.java
@@ -9,8 +9,11 @@ class PointageTest {
 
     @Test
     public void testVerifyQuota() {
-        var pointage = new Pointage("Enseigner", 1,0.5);
-        assertEquals("Le quota du temps alloué doit être égal à 1",);
+        var pointage1 = new Pointage("Enseigner", 1,1.4);
+        var pointage2 = new Pointage("Surveiller",2,1);
+
+        assertEquals("Le quota du temps alloué doit être égal à 1", pointage1.verifyQuota());
+        assertEquals(true,pointage2.verifyQuota());
 
     }
 
